f0897737714b7cd2cfaa5d8cd2e7c2657656774c
Frode <frodeluren@gmail.com>
2020-05-02 18:19:29 +0200

    Commented on form structure service

--- src/main/java/net/vesseldoc/server/service/FormStructureService.java
+++ src/main/java/net/vesseldoc/server/service/FormStructureService.java
@@ -1,11 +1,8 @@
 package net.vesseldoc.server.service;
 
-import net.vesseldoc.server.model.Form;
 import net.vesseldoc.server.model.FormStructure;
 import net.vesseldoc.server.repository.FormStructureRepository;
-import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.provider.HibernateUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -21,6 +18,11 @@ public class FormStructureService {
     @Autowired
     private FormStructureRepository formStructureRepository;
 
+    /**
+     * Gets a list of all form structures.
+     *
+     * @return list of form structures.
+     */
     public List<List<Object>> getAllInfo() {
         List<FormStructure> dbContent = formStructureRepository.getAll();
         List<List<Object>> list = new ArrayList<List<Object>>();
@@ -32,10 +34,24 @@ public class FormStructureService {
         return list;
     }
 
+    /**
+     * Gets content of a given form structure id.
+     *
+     * @param id form structure id:
+     * @return form structure content as bytearray.
+     */
     public byte[] getContent(long id) {
         return formStructureRepository.getContentById(id);
     }
 
+    /**
+     * Saves a form structure and returns its id.
+     *
+     * @param title form structure title.
+     * @param file form structure content as multipartfile.
+     * @return form structure id.
+     * @throws IOException if file cant be saved.
+     */
     public long saveStructure(String title, MultipartFile file) throws IOException {
         byte[] bytes = file.getBytes();
         FormStructure structure = new FormStructure();
@@ -45,6 +61,12 @@ public class FormStructureService {
         return formStructureRepository.save(structure).getId();
     }
 
+    /**
+     * Gets form structure with info from database.
+     *
+     * @param id form structure id.
+     * @return form structure object.
+     */
     public FormStructure getFormStructure(long id) {
         return formStructureRepository.getOne(id);
     }
